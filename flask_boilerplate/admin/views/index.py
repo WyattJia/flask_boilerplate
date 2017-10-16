12ca6d02b0813de57d6b4f17711f994cf249fd78
贾伟川 <wellschuan@gmail.com>
2017-10-16 13:42:18 +0800

    Removed front end libs && removed admin views

--- flask_boilerplate/admin/views/index.py
+++ flask_boilerplate/admin/views/index.py
@@ -33,32 +33,3 @@ def user_loader(uid):
 def index():
     return render_theme_template(theme, 'index.html')
 
-
-@admin.route('/login', methods=["GET", "POST"])
-def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('admin.index'))
-
-    form = LoginForm()
-    if request.method == "POST" and form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-
-        user = User.query.filter(or_(User.username == username, User.email == username)).first()
-
-        if user and user.is_active():
-            if verify_password(current_app.config['SECRET_KEY'], password, user.password):
-                login_user(user, remember=form.remember_me.data)
-                return redirect(request.args.get('next') or url_for('admin.index'))
-            else:
-                form.password.errors.append(u'您输入的密码不正确')
-        else:
-            form.username.errors.append(u'用户名或邮箱不正确')
-    return render_theme_template(theme, 'login.html', form=form)
-
-
-@admin.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('admin.login'))
